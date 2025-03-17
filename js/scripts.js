592efb3569a119c0088653073bd20bf0f5c514b1
caamomilaa <163582100+caamomilaa@users.noreply.github.com>
2025-03-17 11:36:22 +0100

    update

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+// function printOne() {
+//     console.log(1);}
+
+// printOne();
+
+
+function printNumber(number){
+    console.log(number);
+}
+
+printNumber(1)
+printNumber(2)
+printNumber(3)
